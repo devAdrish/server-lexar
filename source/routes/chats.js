a916d1518f03a6690f0dc3d1a1287ade9eadc55c
adrish <adrish.shahid@retailo.co>
2022-09-13 01:23:17 +0500

    update getChat route

--- source/routes/chats.js
+++ source/routes/chats.js
@@ -8,12 +8,28 @@ const preparedResponse = new Response();
 router.get("/chat/:user1/:user2", async (req, res) => {
   try {
     const { user1, user2 } = req.params;
+    const { from, to } = req.query;
     if (!user1 || !user2)
       return res.status(404).send(preparedResponse.error("Incomplete Data"));
     const userArr = [user1, user2].sort();
     const chatId = `${userArr[0]}&${userArr[1]}`;
     const chat = await Chat.findOne({ chatId }).exec();
-    return res.status(200).send(preparedResponse.success(chat));
+    const { messages, users } = chat;
+    let list = [];
+    if (from && to) {
+      const limit = to <= messages.length ? to : messages.length;
+      list = messages.reverse().slice(from, limit).reverse();
+    } else list = messages.slice(-40);
+    return res
+      .status(200)
+      .send(
+        preparedResponse.success({
+          chatId,
+          users,
+          messages: list,
+          total: messages.length,
+        })
+      );
   } catch (err) {
     return res.status(500).send(preparedResponse.serverError(err.toString()));
   }
